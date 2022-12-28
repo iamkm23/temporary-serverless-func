d82e27b331df1d5ac19b36668b9b39fcbe14e9aa
km <kyawmyo.oo@outlook.com>
2022-12-28 20:21:25 +0800

    CORS Error Fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,8 +1,11 @@
-const items = require('../assets/data')
+const items = require("../assets/data");
 
 exports.handler = async (event, context, cb) => {
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
-    body: JSON.stringify(items)
+    body: JSON.stringify(items),
   };
 };
